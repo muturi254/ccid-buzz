44613a649f507235da877fa8bb58cbe3c183f4e0
muturi254 <petermuturi74@gmail.com>
2019-06-08 13:26:31 +0300

    automating docker build

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ def generate_buzz():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug= True, port=int(os.getenv('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
